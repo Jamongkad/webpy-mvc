add51e43eb6fb8b8a5a80d816c096eb14f4936f6
Mathew <mathew@mathew-laptop.(none)>
2010-06-29 18:08:30 +0800

    fixed header

--- sunguppy/modules/header/controllers/header.py
+++ sunguppy/modules/header/controllers/header.py
@@ -18,4 +18,8 @@ sa = SprocketAuth(app)
 
 class index(object):
     def GET(self):
-        return render('header.mako', login=LoginAccountForm(), create=CreateAccountForm(), masthead=masthead.index().GET('Mathew'))
+        login = LoginAccountForm()
+        create = CreateAccountForm()
+        mast = masthead.index().GET('Mathew')
+        logged_in = web.ctx.session.loggedIn
+        return render('header.mako', login=login, create=create, mast=mast, logged_in=logged_in)
